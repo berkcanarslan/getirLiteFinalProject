cca42f84461bfdfbf21b25fe4add00f9e3899d39
Berkcan Arslan <arslanberkcan06@gmail.com>
2024-04-24 23:11:23 +0300

    always on light mode added

--- GetirLiteFinalProject/SceneDelegate.swift
+++ GetirLiteFinalProject/SceneDelegate.swift
@@ -19,6 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: scene.coordinateSpace.bounds)
         window?.windowScene = scene
+        window?.overrideUserInterfaceStyle = .light
         let navigationController = UINavigationController(rootViewController: ViewController())
         
         window?.rootViewController = navigationController
